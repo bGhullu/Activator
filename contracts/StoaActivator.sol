59de3d79416bb5575ec8bf3d58829fe0f82f6421
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2022-10-25 17:29:05 +0100

    some changes

--- contracts/StoaActivator.sol
+++ contracts/StoaActivator.sol
@@ -4,41 +4,52 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-error Activator__NotAuthorizedUser();
-error Activator__NotAllowedZeroValue();
+error StoaActivator__NotAuthorizedUser();
+error StoaActivator__NotAllowedZeroValue();
+error StoaActivator__NotValidAmount();
 
-contract Activator {
+contract StoaActivator {
     struct DepositSyntheticAsset {
         address depositer;
-        address token;
         uint256 amount;
     }
 
     event DepositRequest(address user, address synToken, uint256 amount);
     event Withdraw(address user, address synToken, uint256 amount);
 
-    DepositSyntheticAsset[] private depositToActivator;
+    address private immutable i_synToken;
+    address private immutable i_underlying;
 
-    function deposit(address synToken, uint256 amount) external {
-        if (amount == 0) {
-            revert Activator__NotAllowedZeroValue();
-        }
-        IERC20(synToken).transferFrom(msg.sender, address(this), amount);
-        DepositSyntheticAsset memory depositAsset = DepositSyntheticAsset(
-            msg.sender,
-            synToken,
-            amount
-        );
-        depositToActivator.push(depositAsset);
-        emit DepositRequest(msg.sender, synToken, amount);
+    DepositSyntheticAsset[] private depositToStoaActivator;
+
+    constructor(address synToken, address underlying) {
+        i_synToke = synToken;
+        i_underlying = underlying;
     }
 
-    function withdraw(address synToken, uint256 amount) external {
+    function deposit(uint256 amount) external {
+        if (amount == 0) {
+            revert StoaActivator__NotAllowedZeroValue();
+        }
+        IERC20(i_synToken).transferFrom(msg.sender, address(this), amount);
+        DepositSyntheticAsset memory depositAsset = DepositSyntheticAsset(
+            msg.sender,
+            amount
+        );
+        depositToStoaActivator.push(depositAsset);
+        emit DepositRequest(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) external {
         DepositSyntheticAsset[]
-            memory _depositSyntheticAsset = depositToActivator;
+            memory _depositSyntheticAsset = depositToStoaActivator;
         for (uint i = 0; i < _depositSyntheticAsset.length; i++) {
             if (!_depositSyntheticAsset[i].depositer == msg.sender) {
-                revert Activator__NotAuthorizedUser();
+                revert StoaActivator__NotAuthorizedUser();
+            }
+            uint index = i;
+            if (_depositSyntheticAsset[index].amount < amount) {
+                revert StoaActivator__NotValidAmount();
             }
             IERC20(synToken).tranfer(address(this), msg.sender, amount);
             emit Withdraw(msg.sender, synToken, amount);
